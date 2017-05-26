527abf7cccfd7ad5812ce303bc909124be8a9fe7
Laurence Cook <lwc24@cam.ac.uk>
2017-05-26 18:56:55 +0100

    dist for v0.2

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='horsetailmatching',
-        version='0.1',
+        version='0.2',
         description='A method for optimization under uncertainty',
         url='https://github.com/lwcook/horsetail-matching',
-        download_url='https://github.com/lwcook/horsetail-matching/archive/0.1.targ.gz',
+        download_url='https://github.com/lwcook/horsetail-matching/archive/0.2.targ.gz',
         author='Laurence W. Cook',
         author_email='lwc24@cam.ac.uk',
         license='MIT',
