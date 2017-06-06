bf01610bc0c5fb999d34dded3bfb6f1c0890ac06
Laurence Cook <lwc24@cam.ac.uk>
2017-06-06 11:15:01 +0100

    Publishing to PyPI

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='horsetailmatching',
         version='0.3',
         description='A method for optimization under uncertainty',
-        url='https://github.com/lwcook/horsetail-matching',
+        url='https://www-edc.eng.cam.ac.uk/aerotools/horsetailmatching',
         download_url='https://github.com/lwcook/horsetail-matching/archive/0.3.targ.gz',
         author='Laurence W. Cook',
         author_email='lwc24@cam.ac.uk',
