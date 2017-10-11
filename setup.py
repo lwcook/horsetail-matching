df4b3a53be31ec1363a4834dd7e2a67ea15d94b8
Laurence Cook <lwc24@cam.ac.uk>
2017-10-11 16:10:38 +0100

    Latest version

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='horsetailmatching',
-        version='1.5.2',
+        version='1.6',
         url='https://www-edc.eng.cam.ac.uk/aerotools/horsetailmatching',
-        download_url='https://github.com/lwcook/horsetail-matching/archive/1.5.2.tar.gz',
+        download_url='https://github.com/lwcook/horsetail-matching/archive/1.6.tar.gz',
         author='Laurence W. Cook',
         author_email='lwc24@cam.ac.uk',
         install_requires=['numpy >= 1.12.1'],
